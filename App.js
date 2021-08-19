337f2cdd7fe29245077223606b970a172b32d5aa
Pedro Martins <pmartins@manwinwin.com>
2021-08-19 09:37:46 +0100

    add Card to restaurant info

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import {StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 //const isAndroid = Platform.OS === 'android';
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight}}>
-        <View style={{ padding: 16, backgroundColor: 'green' }}>
-          <Text>search</Text>
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue',flexGrow:1 }}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
-      <ExpoStatusBar style='auto'/>
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-});
+// const styles = StyleSheet.create({});
---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Card, Paragraph } from "react-native-paper";
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    openingHours,
+    rating = 4,
+    isClosedTemporary,
+  } = restaurant;
+
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} source={{ uri: photos[0] }} style={styles.cover} />
+      <Card.Content>
+        <Paragraph>{name}</Paragraph>
+      </Card.Content>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+  },
+  cover: {
+    padding: 16,
+    backgroundColor: "white",
+  },
+});
