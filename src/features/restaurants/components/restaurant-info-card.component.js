f17edefc8ac5851dece8fa03c645c34248d3bbb6
Pedro Martins <pmartins@manwinwin.com>
2021-08-19 11:28:33 +0100

    rating

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,10 +2,18 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { Card, Paragraph } from "react-native-paper";
 import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
 
 const Title = styled.Text`
-font-family: ${(props) => props.theme.fonts.body}
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
 const RestaurantCard = styled(Card)`
@@ -15,6 +23,12 @@ const RestaurantCard = styled(Card)`
 const RestaurantCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.bg.primary};
   padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -31,11 +45,19 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     isClosedTemporary,
   } = restaurant;
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Card.Content>
         <Title>{name}</Title>
+        <Rating>
+          {ratingArray.map((v, i) => (
+            <SvgXml key={i} xml={star} width={20} height={20} />
+          ))}
+        </Rating>
+        <Address>{address}</Address>
       </Card.Content>
     </RestaurantCard>
   );
