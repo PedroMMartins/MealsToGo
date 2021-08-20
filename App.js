44e7e4f190f9265a2b259bb3bceea0f1d7fbf112
Pedro Martins <pmartins@manwinwin.com>
2021-08-20 14:13:31 +0100

    tab navigation

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
-import { ThemeProvider } from "styled-components";
 import { theme } from "./src/infrastructure/theme";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { View } from "react-native";
+import { Text } from "./src/components/typography/text.component";
+import { SafeArea } from "./src/components/utility/safe-area.component";
 
 //const isAndroid = Platform.OS === 'android';
 export default function App() {
@@ -23,12 +28,29 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+  const Settings = () => (
+    <SafeArea>
+      <Text>Settings</Text>
+    </SafeArea>
+  );
+  const Map = () => (
+    <SafeArea>
+      <Text>Map</Text>
+    </SafeArea>
+  );
+  const Tab = createBottomTabNavigator();
 
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
-        <ExpoStatusBar style="auto" />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
+        {/* <ExpoStatusBar style="auto" /> */}
       </ThemeProvider>
     </>
   );
