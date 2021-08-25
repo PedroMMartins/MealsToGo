85115b0757403a712518e0d0802040f3fe2646a7
Pedro Martins <pmartins@manwinwin.com>
2021-08-25 12:44:33 +0100

    fix android bug - images on favourites

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -3,5 +3,5 @@ import { Image, View } from "react-native";
 import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurantInfo.component";
 
 export const MapCallout = ({ restaurant }) => {
-  return <CompactRestaurantInfo restaurant={restaurant} />;
+  return <CompactRestaurantInfo restaurant={restaurant} isMap />;
 };
