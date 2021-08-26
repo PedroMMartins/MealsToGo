1c8831350a363798cff5e4184b55841edc9f4247
Pedro Martins <pmartins@manwinwin.com>
2021-08-26 16:50:39 +0100

    added animation

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -3,16 +3,27 @@ import {
   AccountBackground,
   AccountContainer,
   AccountCover,
+  AnimationWrapper,
   AuthButton,
   Title,
 } from "../components/account.styles";
 
 import { Spacer } from "../../../components/spacer/spacer.component";
+import LottieView from "lottie-react-native";
 
 export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AnimationWrapper>
+        <LottieView
+          key="animation"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/watermelon.json")}
+        />
+      </AnimationWrapper>
       <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthButton
