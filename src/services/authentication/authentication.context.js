f371ba6138f0f87eb8403e98568a014d74598279
Pedro Martins <pmartins@manwinwin.com>
2021-08-26 11:32:28 +0100

    register and fix isAuthenticated

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -19,13 +19,34 @@ export const AuthenticationContextProvider = ({ children }) => {
       })
       .finally(() => setIsLoading(false));
   };
+
+  const onRegister = (email, password, repeatedPassword) => {
+    if (password !== repeatedPassword) {
+      setError("Error:Passwords do not match");
+      return;
+    }
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((u) => {
+        setUser(u);
+      })
+      .catch((e) => {
+        setError(e.toString());
+      })
+      .finally(() => setIsLoading(false));
+  };
+
   return (
     <AuthenticationContext.Provider
       value={{
+        //if there is a user object than is authenticated
+        isAuthenticated: !!user,
         user,
         isLoading,
         error,
         onLogin,
+        onRegister,
       }}
     >
       {children}
